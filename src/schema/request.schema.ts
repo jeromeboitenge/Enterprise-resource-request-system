733ee275920336593b632d0ce1818edf831fe105
boitenge <jeromeboitenge@gmail.com>
2025-12-26 15:12:59 +0200

    validations

---
+++ src/schema/request.schema.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export const createRequestSchema = Joi.object({
+    resourceName: Joi.string().min(3).required(),
+    description: Joi.string().optional(),
+    amountRequested: Joi.number().positive().required(),
+    departmentId: Joi.string().required()
+});
+
+export const updateRequestStatusSchema = Joi.object({
+    status: Joi.string().valid('approved', 'rejected').required(),
+    comment: Joi.string().optional()
+});
