aa08d8c1da33aacc25fdc565f202d39fd13ef972
boitenge <jeromeboitenge@gmail.com>
2025-12-26 15:07:03 +0200

    adding validations

---
+++ src/middleware/validate.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { ObjectSchema } from 'joi';
+
+export const validate =
+    (schema: ObjectSchema, property: 'body' | 'params' = 'body') =>
+        (req: Request, res: Response, next: NextFunction) => {
+            const { error } = schema.validate(req[property], { abortEarly: false });
+
+            if (error) {
+                return res.status(400).json({
+                    message: 'Validation error',
+                    errors: error.details.map(d => d.message)
+                });
+            }
+
+            next();
+        };
