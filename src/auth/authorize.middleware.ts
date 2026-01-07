55249e8d7e86201f728180b46c2379a1c42b3744
boitenge <jeromeboitenge@gmail.com>
2026-01-07 16:18:24 +0200

    summarizing codes

--- src/auth/authorize.middleware.ts
+++ src/auth/authorize.middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const authorize = (allowedRoles: string[]) => {
+export const authorize = (...allowedRoles: string[]) => {
     return (req: Request, res: Response, next: NextFunction): void => {
         if (!req.user || !allowedRoles.includes(req.user.role)) {
             res.status(403).json({
