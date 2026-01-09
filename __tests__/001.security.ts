73e837db4b913617cea2b68d8700f10494120c61
boitenge <jeromeboitenge@gmail.com>
2026-01-09 16:39:40 +0200

    testing compare password

--- __tests__/001.security.ts
+++ __tests__/001.security.ts
@@ -2,14 +2,14 @@ import { expect, test, describe } from '@jest/globals'
 import { comparePassword, hashPassword } from '../src/utils/Security'
 
 describe('security ', () => {
-    test('hashing password returns string', () => {
+    test('hashing password returns string', async () => {
         const password = 'Boitenge12!'
         const hashedPassword = hashPassword(password)
         expect(typeof (hashedPassword)).toBe('string')
         expect(hashedPassword.length).toBe(60)
         expect(hashPassword).not.toBe(password)
     })
-    test('hashing same passwords returns string', () => {
+    test('hashing same passwords returns string', async () => {
         const password = "boitenge12"
         const hash1 = hashPassword(password)
         const hash2 = hashPassword(password)
@@ -17,9 +17,15 @@ describe('security ', () => {
     })
 })
 describe('comparePasswords', () => {
-    test('compare password', () => {
+    test('compare password', async () => {
         const password = 'booitenge';
         const hashed = hashPassword(password)
-        expect(comparePassword(hashed, password)).toBeTruthy()
+        expect(comparePassword(hashed, password)).toBe(true)
+    })
+    test('returns false for different password', async () => {
+        const password = 'Password'
+        const wrongPassword = 'password'
+        const hashed = hashPassword(password)
+        expect(comparePassword(hashed, wrongPassword)).toBe(false)
     })
 })
